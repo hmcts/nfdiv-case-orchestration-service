a7d010ee806c5709fe5f8d955521c6d362c4ab3c
Shaun Francis <shaun.francis1@hmcts.net>
2021-01-08 12:02:22 +0000

    Use nfdiv-common-lib removeAllPetitionDocuments

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/RemoveMiniPetitionDraftDocumentsTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/RemoveMiniPetitionDraftDocumentsTask.java
@@ -2,24 +2,24 @@ package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.divorce.orchestration.client.CaseFormatterClient;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
+import uk.gov.hmcts.reform.divorce.service.CaseFormatterService;
 
 import java.util.Map;
 
 @Component
 public class RemoveMiniPetitionDraftDocumentsTask implements Task<Map<String, Object>> {
 
-    private final CaseFormatterClient caseFormatterClient;
+    private final CaseFormatterService caseFormatterService;
 
     @Autowired
-    public RemoveMiniPetitionDraftDocumentsTask(final CaseFormatterClient caseFormatterClient) {
-        this.caseFormatterClient = caseFormatterClient;
+    public RemoveMiniPetitionDraftDocumentsTask(final CaseFormatterService caseFormatterService) {
+        this.caseFormatterService = caseFormatterService;
     }
 
     @Override
     public Map<String, Object> execute(final TaskContext context, final Map<String, Object> caseData) {
-        return caseFormatterClient.removeAllPetitionDocuments(caseData);
+        return caseFormatterService.removeAllPetitionDocuments(caseData);
     }
 }
