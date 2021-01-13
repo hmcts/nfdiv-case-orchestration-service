dc9127ccdf056cb2c2cd9d45a9ec27321602237b
Shaun Francis <shaun.francis1@hmcts.net>
2021-01-13 12:22:08 +0000

    Convert CaseDataToDivorceFormatterTask

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/RetrieveCaseITest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/RetrieveCaseITest.java
@@ -5,20 +5,22 @@ import com.github.tomakehurst.wiremock.matching.EqualToPattern;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.divorce.model.UserDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.CaseDataResponse;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.testutil.CourtsMatcher;
+import uk.gov.hmcts.reform.divorce.service.CaseFormatterService;
 
 import java.util.Collections;
 import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpStatus.OK;
@@ -31,7 +33,6 @@ import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_COURT;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_EMAIL;
-import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_ERROR;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_STATE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_DIVORCE_UNIT;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ObjectMapperTestUtil.convertObjectToJsonString;
@@ -50,6 +51,9 @@ public class RetrieveCaseITest extends IdamTestSupport {
             .caseData(CASE_DATA)
             .build();
 
+    @MockBean
+    private CaseFormatterService caseFormatterService;
+
     @Autowired
     private MockMvc webClient;
 
@@ -66,17 +70,6 @@ public class RetrieveCaseITest extends IdamTestSupport {
     }
 
     @Test
-    public void givenCMSThrowsException_whenRetrieveAosCase_thenPropagateException() throws Exception {
-        stubGetCaseFromCMS(HttpStatus.INTERNAL_SERVER_ERROR, TEST_ERROR);
-
-        webClient.perform(get(API_URL)
-            .header(AUTHORIZATION, AUTH_TOKEN)
-            .accept(APPLICATION_JSON))
-            .andExpect(status().isInternalServerError())
-            .andExpect(content().string(containsString(TEST_ERROR)));
-    }
-
-    @Test
     public void givenNoCaseExists_whenGetCase_thenReturnEmptyResponse() throws Exception {
         stubGetCaseFromCMS(null);
 
@@ -84,19 +77,6 @@ public class RetrieveCaseITest extends IdamTestSupport {
             .header(AUTHORIZATION, AUTH_TOKEN)
             .accept(APPLICATION_JSON))
             .andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void givenCFSThrowsException_whenGetCase_thenPropagateException() throws Exception {
-        stubGetCaseFromCMS(CASE_DETAILS);
-
-        stubFormatterServerEndpoint(HttpStatus.INTERNAL_SERVER_ERROR, TEST_ERROR);
-
-        webClient.perform(get(API_URL)
-            .header(AUTHORIZATION, AUTH_TOKEN)
-            .accept(APPLICATION_JSON))
-            .andExpect(status().isInternalServerError())
-            .andExpect(content().string(containsString(TEST_ERROR)));
     }
 
     @Test
@@ -111,10 +91,11 @@ public class RetrieveCaseITest extends IdamTestSupport {
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     public void givenAllGoesWellProceedAsExpected_RetrieveCaseInformation() throws Exception {
         stubGetCaseFromCMS(CASE_DETAILS);
 
-        stubFormatterServerEndpoint();
+        when(caseFormatterService.transformToDivorceSession(any(Map.class))).thenReturn(CASE_DATA);
 
         CaseDataResponse expected = CaseDataResponse.builder()
             .data(CASE_DATA)
@@ -146,18 +127,5 @@ public class RetrieveCaseITest extends IdamTestSupport {
 
     private void stubGetMultipleCaseFromCMS() {
         stubGetCaseFromCMS(HttpStatus.MULTIPLE_CHOICES, "");
-    }
-
-    private void stubFormatterServerEndpoint() {
-        stubFormatterServerEndpoint(OK, convertObjectToJsonString(CASE_DATA));
-    }
-
-    private void stubFormatterServerEndpoint(HttpStatus status, String message) {
-        formatterServiceServer.stubFor(WireMock.post(FORMAT_TO_DIVORCE_CONTEXT_PATH)
-            .withRequestBody(equalToJson(convertObjectToJsonString(CASE_DATA)))
-            .willReturn(aResponse()
-                .withStatus(status.value())
-                .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                .withBody(message)));
     }
 }
