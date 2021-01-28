1d0a165e0effdd5aa1574bd17bfa801c531ea4ed
Shaun Francis <shaun.francis1@hmcts.net>
2021-01-28 11:44:26 +0000

    Reorganise tests (#31)
    * Move functional tests
    
    * Move mock controller tests to integrationTest module
    
    * Move test resources
    
    * Add group to functional gradle task

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/orchestration/controller/RetrieveCaseITest.java
@@ -0,0 +1,163 @@
+package uk.gov.hmcts.reform.divorce.orchestration.controller;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.matching.EqualToPattern;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.divorce.model.UserDetails;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.CaseDataResponse;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
+import uk.gov.hmcts.reform.divorce.orchestration.testutil.CourtsMatcher;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_COURT;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_EMAIL;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_ERROR;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_STATE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_DIVORCE_UNIT;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ObjectMapperTestUtil.convertObjectToJsonString;
+
+public class RetrieveCaseITest extends IdamTestSupport {
+
+    private static final String API_URL = "/retrieve-case";
+    private static final String GET_CASE_CONTEXT_PATH = "/casemaintenance/version/1/case";
+    private static final String FORMAT_TO_DIVORCE_CONTEXT_PATH = "/caseformatter/version/1/to-divorce-format";
+
+    private static final Map<String, Object> CASE_DATA = Collections.singletonMap(D_8_DIVORCE_UNIT, TEST_COURT);
+    private static final CaseDetails CASE_DETAILS =
+        CaseDetails.builder()
+            .caseId(TEST_CASE_ID)
+            .state(TEST_STATE)
+            .caseData(CASE_DATA)
+            .build();
+
+    @Autowired
+    private MockMvc webClient;
+
+    @Before
+    public void setUp() {
+        stubUserDetailsEndpoint(OK, AUTH_TOKEN, convertObjectToJsonString(UserDetails.builder().email(TEST_EMAIL).build()));
+    }
+
+    @Test
+    public void givenNoAuthToken_whenRetrieveCase_thenReturnBadRequest() throws Exception {
+        webClient.perform(get(API_URL)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenCMSThrowsException_whenRetrieveAosCase_thenPropagateException() throws Exception {
+        stubGetCaseFromCMS(HttpStatus.INTERNAL_SERVER_ERROR, TEST_ERROR);
+
+        webClient.perform(get(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isInternalServerError())
+            .andExpect(content().string(containsString(TEST_ERROR)));
+    }
+
+    @Test
+    public void givenNoCaseExists_whenGetCase_thenReturnEmptyResponse() throws Exception {
+        stubGetCaseFromCMS(null);
+
+        webClient.perform(get(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void givenCFSThrowsException_whenGetCase_thenPropagateException() throws Exception {
+        stubGetCaseFromCMS(CASE_DETAILS);
+
+        stubFormatterServerEndpoint(HttpStatus.INTERNAL_SERVER_ERROR, TEST_ERROR);
+
+        webClient.perform(get(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isInternalServerError())
+            .andExpect(content().string(containsString(TEST_ERROR)));
+    }
+
+    @Test
+    public void givenMultipleCases_whenGetCase_thenPropagateException() throws Exception {
+        stubGetMultipleCaseFromCMS();
+
+        webClient.perform(get(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isMultipleChoices())
+            .andExpect(content().string(""));
+    }
+
+    @Test
+    public void givenAllGoesWellProceedAsExpected_RetrieveCaseInformation() throws Exception {
+        stubGetCaseFromCMS(CASE_DETAILS);
+
+        stubFormatterServerEndpoint();
+
+        CaseDataResponse expected = CaseDataResponse.builder()
+            .data(CASE_DATA)
+            .caseId(TEST_CASE_ID)
+            .state(TEST_STATE)
+            .court(TEST_COURT)
+            .build();
+
+        webClient.perform(get(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(convertObjectToJsonString(expected)))
+            .andExpect(content().string(hasJsonPath("$.data.court", CourtsMatcher.isExpectedCourtsList())));
+    }
+
+    private void stubGetCaseFromCMS(CaseDetails caseDetails) {
+        stubGetCaseFromCMS(OK, convertObjectToJsonString(caseDetails));
+    }
+
+    private void stubGetCaseFromCMS(HttpStatus status, String message) {
+        maintenanceServiceServer.stubFor(WireMock.get(GET_CASE_CONTEXT_PATH)
+            .withHeader(AUTHORIZATION, new EqualToPattern(AUTH_TOKEN))
+            .willReturn(aResponse()
+                .withStatus(status.value())
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .withBody(message)));
+    }
+
+    private void stubGetMultipleCaseFromCMS() {
+        stubGetCaseFromCMS(HttpStatus.MULTIPLE_CHOICES, "");
+    }
+
+    private void stubFormatterServerEndpoint() {
+        stubFormatterServerEndpoint(OK, convertObjectToJsonString(CASE_DATA));
+    }
+
+    private void stubFormatterServerEndpoint(HttpStatus status, String message) {
+        formatterServiceServer.stubFor(WireMock.post(FORMAT_TO_DIVORCE_CONTEXT_PATH)
+            .withRequestBody(equalToJson(convertObjectToJsonString(CASE_DATA)))
+            .willReturn(aResponse()
+                .withStatus(status.value())
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .withBody(message)));
+    }
+}
---
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/orchestration/controller/ValidateBulkCaseListingDataITest.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.divorce.orchestration.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import static java.time.ZoneOffset.UTC;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ResourceLoader.loadResourceAsString;
+
+public class ValidateBulkCaseListingDataITest extends IdamTestSupport {
+
+    private static final String API_URL = "/bulk/validate/listing";
+
+    private static final String REQUEST_JSON_PATH = "jsonExamples/payloads/bulkCaseCcdCallbackRequest.json";
+
+    private static final String TEST_AUTH_TOKEN = "testAuthToken";
+
+    private static final String ERROR_MESSAGE = "Court hearing date is in the past";
+
+    @Autowired
+    private MockMvc webClient;
+
+    @MockBean
+    private Clock clock;
+
+    @Before
+    public void setup() {
+        when(clock.getZone()).thenReturn(UTC);
+    }
+
+    @Test
+    public void givenCallbackRequestWithFutureDateBulkCaseData_thenReturnCallbackResponse() throws Exception {
+        // Mock current date to be in the past compared to the request json
+        LocalDateTime today = LocalDateTime.parse("1999-01-01T10:20:55.000");
+        when(clock.instant()).thenReturn(today.toInstant(ZoneOffset.UTC));
+
+        webClient.perform(MockMvcRequestBuilders.post(API_URL)
+                .header(AUTHORIZATION, TEST_AUTH_TOKEN)
+                .content(loadResourceAsString(REQUEST_JSON_PATH))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.errors", nullValue()));
+    }
+
+    @Test
+    public void givenCallbackRequestWithPastDateBulkCaseData_thenReturnCallbackResponseWithErrors() throws Exception {
+        // Mock current date to be in the past compared to the request json
+        LocalDateTime today = LocalDateTime.parse("2001-01-01T10:20:55.000");
+        when(clock.instant()).thenReturn(today.toInstant(ZoneOffset.UTC));
+
+        webClient.perform(MockMvcRequestBuilders.post(API_URL)
+                .header(AUTHORIZATION, TEST_AUTH_TOKEN)
+                .content(loadResourceAsString(REQUEST_JSON_PATH))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.errors", contains(ERROR_MESSAGE)));
+    }
+}
