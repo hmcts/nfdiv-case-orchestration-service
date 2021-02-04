3595037bcd2564301f8d91ef5d15705e1db65b87
Sarah <sarahjanelittlejohn@gmail.com>
2021-02-04 09:43:38 +0000

    NFDIV-386: UT for throwing exception

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SubmitCaseToCCDWorkflowTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SubmitCaseToCCDWorkflowTest.java
@@ -5,20 +5,24 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.DefaultTaskContext;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.FormatDivorceSessionToCaseDataTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.SubmitCaseToCCD;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Collections.singletonMap;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsMapContaining.hasEntry;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SubmitCaseToCCDWorkflowTest {
@@ -45,6 +49,20 @@ public class SubmitCaseToCCDWorkflowTest {
 
         verify(submitCaseToCCD).execute(any(), eq(incomingPayload));
 
+    }
+
+    @Test(expected = WorkflowException.class)
+    public void whenSubmittingCaseToCCDWorkflow_ShouldThrowException() throws WorkflowException {
+        final DefaultTaskContext context = new DefaultTaskContext();
+        context.setTransientObject(AUTH_TOKEN_JSON_KEY, AUTH_TOKEN);
+
+        Map<String, Object> incomingPayload = new HashMap<>();
+
+        when(submitCaseToCCD.execute(context, incomingPayload)).thenThrow(TaskException.class);
+
+        submitCaseToCCDWorkflow.run(incomingPayload, AUTH_TOKEN);
+
+        verify(submitCaseToCCD).execute(context, incomingPayload);
     }
 
 }
