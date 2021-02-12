dbaf2cd219398f8ba2bc720380dbe8f4b935f950
Nitin Prabhu <prabhunitin61@gmail.com>
2021-02-12 16:16:15 +0000

    Added functional test for get case endpoint

--- src/functionalTest/java/uk/gov/hmcts/reform/divorce/maintenance/GetCaseFromCcdTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/divorce/maintenance/GetCaseFromCcdTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.divorce.model.idam.UserDetails;
 import uk.gov.hmcts.reform.divorce.support.CcdSubmissionSupport;
 import uk.gov.hmcts.reform.divorce.util.RestUtil;
@@ -17,11 +18,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_COURT;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdStates.AWAITING_PAYMENT;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_PETITIONER_EMAIL;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.STATE_CCD_FIELD;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.courts.CourtConstants.SELECTED_COURT_KEY;
 
 public class GetCaseFromCcdTest extends CcdSubmissionSupport {
-
-    private static final String PAYLOAD_CONTEXT_PATH = "fixtures/retrieve-case/";
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -31,7 +35,17 @@ public class GetCaseFromCcdTest extends CcdSubmissionSupport {
 
     @Test
     public void givenCaseExists_whenRetrieveCase_thenReturnResponse() throws Exception {
+        UserDetails userDetails = createCitizenUser();
 
+        CaseDetails caseDetails = submitCase("submit-complete-case.json", userDetails,
+            Pair.of(D_8_PETITIONER_EMAIL, userDetails.getEmailAddress()));
+
+        Response cosResponse = getCase(userDetails.getAuthToken());
+
+        assertEquals(HttpStatus.OK.value(), cosResponse.getStatusCode());
+        assertEquals(String.valueOf(caseDetails.getId()), cosResponse.path(CASE_ID_JSON_KEY));
+        assertEquals(TEST_COURT, cosResponse.path(SELECTED_COURT_KEY));
+        assertEquals(AWAITING_PAYMENT, cosResponse.path(STATE_CCD_FIELD));
     }
 
     @Test
@@ -47,6 +61,15 @@ public class GetCaseFromCcdTest extends CcdSubmissionSupport {
 
         assertEquals(HttpStatus.MULTIPLE_CHOICES.value(), cosResponse.getStatusCode());
         assertEquals(cosResponse.getBody().asString(), "");
+    }
+
+    @Test
+    public void givenNoCaseExistsInCcd_whenGetCase_thenReturn404() {
+        UserDetails userDetails = createCitizenUser();
+
+        Response cosResponse = getCase(userDetails.getAuthToken());
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), cosResponse.getStatusCode());
     }
 
     private Response getCase(String userToken) {
