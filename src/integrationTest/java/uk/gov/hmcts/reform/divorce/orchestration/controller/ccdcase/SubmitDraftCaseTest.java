d296e241dcb0e1e73f0ccf7ceea7c956a6347c3a
Sarah <sarahjanelittlejohn@gmail.com>
2021-02-17 15:40:05 +0000

    NFDIV-471: integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/orchestration/controller/ccdcase/SubmitDraftCaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/orchestration/controller/ccdcase/SubmitDraftCaseTest.java
@@ -5,13 +5,16 @@ import com.github.tomakehurst.wiremock.matching.EqualToPattern;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.divorce.model.ccd.CoreCaseData;
 import uk.gov.hmcts.reform.divorce.model.usersession.DivorceSession;
 import uk.gov.hmcts.reform.divorce.orchestration.controller.MockedFunctionalTest;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
@@ -32,6 +35,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_COURT;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_STATE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_DIVORCE_UNIT;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.SUCCESS_STATUS;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.DataTransformationTestHelper.getExpectedTranslatedDraftCoreCaseData;
@@ -41,8 +47,15 @@ import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ObjectMapperTes
 
 public class SubmitDraftCaseTest extends MockedFunctionalTest {
 
-    private static final String DRAFT_API_URL = "/case";
     private static final String SUBMISSION_CONTEXT_PATH = "/case";
+    private static final Map<String, Object> CASE_DATA = Collections.singletonMap(D_8_DIVORCE_UNIT, TEST_COURT);
+    private static final CaseDetails CASE_DETAILS =
+        CaseDetails.builder()
+            .caseId(TEST_CASE_ID)
+            .state(TEST_STATE)
+            .caseData(CASE_DATA)
+            .build();
+
 
     private DivorceSession testDivorceSessionData;
     private DivorceSession testDivorceSessionDataDraft;
@@ -62,8 +75,9 @@ public class SubmitDraftCaseTest extends MockedFunctionalTest {
     public void shouldSubmitDraftCase() throws Exception {
 
         stubMaintenanceServerEndpointForDraftSubmit(singletonMap(ID, TEST_CASE_ID));
+        stubMaintenanceServerEndpointForRetrieveExisting(OK, null);
 
-        final MvcResult result = webClient.perform(post(DRAFT_API_URL)
+        final MvcResult result = webClient.perform(post(SUBMISSION_CONTEXT_PATH)
             .header(AUTHORIZATION, AUTH_TOKEN)
             .content(convertObjectToJsonString(testDivorceSessionDataDraft))
             .contentType(APPLICATION_JSON)
@@ -83,7 +97,7 @@ public class SubmitDraftCaseTest extends MockedFunctionalTest {
 
     @Test
     public void shouldReturnBadRequestIfNoAuthenticationToken() throws Exception {
-        webClient.perform(post(DRAFT_API_URL)
+        webClient.perform(post(SUBMISSION_CONTEXT_PATH)
             .content(convertObjectToJsonString(testDivorceSessionData))
             .contentType(APPLICATION_JSON)
             .accept(APPLICATION_JSON))
@@ -92,7 +106,20 @@ public class SubmitDraftCaseTest extends MockedFunctionalTest {
 
     @Test
     public void shouldReturnBadRequestIfNoPayload() throws Exception {
-        webClient.perform(post(DRAFT_API_URL)
+        webClient.perform(post(SUBMISSION_CONTEXT_PATH)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldReturnBadRequestIfDuplicateCase() throws Exception {
+
+        stubMaintenanceServerEndpointForDraftSubmit(singletonMap(ID, TEST_CASE_ID));
+        stubMaintenanceServerEndpointForRetrieveExisting(OK, convertObjectToJsonString(CASE_DETAILS));
+
+        webClient.perform(post(SUBMISSION_CONTEXT_PATH)
             .header(AUTHORIZATION, AUTH_TOKEN)
             .contentType(APPLICATION_JSON)
             .accept(APPLICATION_JSON))
@@ -110,5 +137,14 @@ public class SubmitDraftCaseTest extends MockedFunctionalTest {
                 .withStatus(OK.value())
                 .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .withBody(convertObjectToJsonString(response))));
+    }
+
+    private void stubMaintenanceServerEndpointForRetrieveExisting(HttpStatus status, String message) {
+        maintenanceServiceServer.stubFor(WireMock.get(SUBMISSION_CONTEXT_PATH)
+            .withHeader(AUTHORIZATION, new EqualToPattern(AUTH_TOKEN))
+            .willReturn(aResponse()
+                .withStatus(status.value())
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .withBody(message)));
     }
 }
