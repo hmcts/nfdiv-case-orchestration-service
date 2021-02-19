99d8c4a33d05a9f933a6a5fa107c19e52a91c44e
Nitin Prabhu <prabhunitin61@gmail.com>
2021-02-19 14:52:43 +0000

    Removed logger added for debugging

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CaseServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CaseServiceImpl.java
@@ -79,7 +79,6 @@ public class CaseServiceImpl implements CaseService {
 
         List<CaseDetails> caseDetailsList = getCaseListForUser(user);
 
-        log.info("case id {} ", caseDetailsList.get(0).getId());
         log.info("Case list size {} after retrieving case for user id {}",
             caseDetailsList.size(),
             user.getUserDetails().getId()
